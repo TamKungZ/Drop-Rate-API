679b97093fcd1ce8a45b9c2e753f514df1392b97
TamKungZ_ <kittiwut.pimpromma@gmail.com>
2025-03-01 11:41:47 +0700

    Update

--- src/main/java/th/tamkungz/droprateapi/api/DropRateAPI.java
+++ src/main/java/th/tamkungz/droprateapi/api/DropRateAPI.java
@@ -1,3 +1,21 @@
+/*
+ * Drop Rate API Mod - Custom Mob Drops & API
+ * Copyright (C) 2024 TamKungZ_
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package th.tamkungz.droprateapi.api;
 
 import net.minecraft.item.Item;
@@ -8,10 +26,16 @@ public class DropRateAPI {
     // Change Map to store a List of DropData
     private static final Map<EntityType<?>, List<DropData>> dropRules = new HashMap<>();
 
+    // Keep track of mobs registered from the config file
+    private static final Set<EntityType<?>> configRegisteredMobs = new HashSet<>();
+
     // Add DropData to the list instead of replacing
     public static void registerDrop(EntityType<?> mob, int rate, List<Item> items, int minAmount, int maxAmount) {
         DropData newDrop = new DropData(rate, items, minAmount, maxAmount);
         dropRules.computeIfAbsent(mob, k -> new ArrayList<>()).add(newDrop);
+
+        // Track mobs added via the config file (only if it's a config-based registration)
+        configRegisteredMobs.add(mob);
         
         System.out.println("Registered Drop: Mob=" + mob + ", Rate=" + rate + 
             ", Items=" + items + ", Amount=" + minAmount + "-" + maxAmount);
@@ -36,6 +60,14 @@ public class DropRateAPI {
             this.maxAmount = maxAmount;
         }
     }
+
+    public static void clearConfigBasedDrops() {
+        for (EntityType<?> mob : configRegisteredMobs) {
+            dropRules.remove(mob);
+            System.out.println("[DropRate] Removed config-based drops for: " + mob);
+        }
+        configRegisteredMobs.clear(); // Reset tracking for the next config load
+    }    
 
     // Remove DropData from the list
     public static void removeDrop(EntityType<?> mob, Item item) {
--- src/main/java/th/tamkungz/droprateapi/config/DropRateConfigLoader.java
+++ src/main/java/th/tamkungz/droprateapi/config/DropRateConfigLoader.java
@@ -1,3 +1,21 @@
+/*
+ * Drop Rate API Mod - Custom Mob Drops & API
+ * Copyright (C) 2024 TamKungZ_
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package th.tamkungz.droprateapi.config;
 
 import com.google.gson.JsonArray;
@@ -30,6 +48,9 @@ public class DropRateConfigLoader {
         }
     
         try (Reader reader = Files.newBufferedReader(CONFIG_PATH)) {
+            // Remove ONLY the drops that were registered from the config file, not other mods' drops
+            DropRateAPI.clearConfigBasedDrops();
+
             JsonObject json = new JsonParser().parse(reader).getAsJsonObject();
             JsonArray configArray = json.getAsJsonArray("config");
     
